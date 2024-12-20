c81e12da65fda4bdd5f59949322a73e6b665ddd2
Etnos De Jesus Santos <profissionaletnos@gmail.com>
2024-12-20 00:00:45 -0300

    1
    1

--- Pipelines (2)/Pipelines/projeto_site/Project_Description.py
+++ Pipelines (2)/Pipelines/projeto_site/Project_Description.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Insurance Prediction",
-    page_icon="teste_plano_saude/Pipelines/img/stethoscope.png"
+    page_icon=".\stethoscope.png"
 )
 
 st.sidebar.header('Project Description')
@@ -10,7 +10,7 @@ st.sidebar.header('Project Description')
 st.write("# Welcome to the Insurance Prediction App 🩺")
 st.write("\n\n")
 
-st.image(r'C:\Users\Shiry\OneDrive\Área de Trabalho\Pipelines - Material de apoio\teste_plano_saude\Pipelines (2)\Pipelines\img\health_insurance_img.jpg')
+st.image('teste_plano_saude\Pipelines (2)\Pipelines\img\health_insurance_img.jpg')
 
 st.write("\n\n")
 
